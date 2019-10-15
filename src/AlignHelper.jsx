1640656338640f21cf227c91984b43dc79b5a8d2
unknown <krazybubbler@gmail.com>
2019-10-15 16:05:58 +0200

    refactored

---
+++ src/AlignHelper.jsx
@@ -0,0 +1,68 @@
+import * as THREE from 'three'
+import { Vector2 } from 'three';
+
+class AlignHelper  {
+    
+    deltaVec = new THREE.Vector3(1,0,1);
+    totalCount = 0;
+    count = 0;
+    cur = new THREE.Vector3();
+    num = new THREE.Vector3();
+    alternateRows = true;
+  
+    constructor(deltaVec, amountVec) {
+        this.num = amountVec;
+        this.deltaVec = deltaVec;
+        this.totalCount = amountVec.x * amountVec.y * amountVec.z;
+    }
+
+    getPositions() {
+        let o = [];
+        for (let i = 0 ; i < this.totalCount ; i++) {
+            o.push(this.getNextPosition());
+        }
+        
+        // center array
+        const center = o[this.totalCount - 1].clone().divideScalar(2);
+        
+        for (let i = 0 ; i < this.totalCount ; i++) {
+            
+            o[i] = o[i].clone().sub(center);
+
+            if (this.alternateRows === true && (i % 2 === 0)) 
+            {
+                o[i] = o[i].clone().sub(new THREE.Vector3(0,0,this.deltaVec.z/2));// this.cur.sub(new THREE.Vector3(0,0,this.deltaVec.z/2));
+            }
+
+            
+        }
+        return o;
+    }
+    getNextPosition() {
+        if (this.count++ > this.totalCount) {
+            throw new Error("Maximum instances reached");
+        }
+        let p = new THREE.Vector3();
+
+        p = this.deltaVec.clone().multiply(this.cur);
+        
+
+        if (++this.cur.x < this.num.x) {
+        }
+        else {
+            this.cur.x = 0;
+            this.cur.y++;
+        }
+
+        if (this.cur.y < this.num.y) {
+
+        }
+        else {
+            this.cur.y = 0;
+            this.cur.z++;
+        }
+       return p; 
+    }
+}
+ 
+export default AlignHelper;
---
+++ src/TweenableMesh.jsx
@@ -0,0 +1,136 @@
+import * as THREE from 'three'
+ 
+import AlignHelper from './AlignHelper'; 
+
+
+THREE.InstancedMesh.prototype.getMatrixAt = function (index) {
+    let m = [];
+    m[0] = this.instanceMatrix.array[index * 16];
+    m[1] = this.instanceMatrix.array[index * 16 + 1];
+    m[2] = this.instanceMatrix.array[index * 16 + 2];
+    m[3] = this.instanceMatrix.array[index * 16 + 3];
+    return m;
+}
+
+class TweenableMesh {
+    scale = new THREE.Vector3(1, 1, 1);
+
+    gapX = 3;
+    gapZ = 4;
+    meshes = [];
+
+    qTweens = []
+    qTweensMap = []
+    posTweens = []
+    posTweensMap = [];
+    dummy = new THREE.Object3D();
+    initialArrangement = [];
+    constructor(scene, meshes, arrangement) {
+        const models = meshes.length ? meshes : [meshes]
+
+        // this.arrangeCountVec = new THREE.Vector3(3, 3, 1);    // how many instances in each axis
+        // this.gapVec = new THREE.Vector3(1, 1, 1);
+
+        // this.amount = this.arrangeCountVec.x * this.arrangeCountVec.y * this.arrangeCountVec.z;
+
+        this.amount = arrangement.length;
+        
+        this.initialArrangement = arrangement;
+
+        let g = new THREE.Group();
+
+        
+        for (let i = 0; i < models.length; i++) {
+            // operate on clone
+            const m = models[i].geometry.clone();
+            
+            let im = new THREE.InstancedMesh(
+                m,
+                models[i].material.clone(),
+                this.amount
+            );
+            g.add(im);
+            this.meshes.push(im);
+        }
+
+        scene.add(g);
+    }
+
+    setScale(scale) {
+        this.scale = new THREE.Vector3(scale, scale, scale);
+
+        for (let i = 0; i < this.meshes.length; i++) {
+            for (let j = 0; j < this.amount; j++) {
+                // this.dummy.matrix.set(this.meshes[i].)
+                var m = this.meshes[i].getMatrixAt(j);
+                this.dummy.matrix.set(m)
+                this.dummy.scale.set(this.scale.x, this.scale.y, this.scale.z)
+                this.dummy.updateMatrix();
+                this.meshes[i].setMatrixAt(j, this.dummy.matrix)
+                // this.meshes[i].scale.set(scale.x, scale.y, scale.z);
+                // this.meshes[i].instanceMatrix.needsUpdate = true;
+            }
+            this.meshes[i].instanceMatrix.needsUpdate = true;
+        }
+
+    }
+
+    setPositionTweens(tweens, map) {
+        this.posTweens = tweens;
+
+        this.posTweensMap = [];
+        for (let i = 0; i < this.amount && tweens.length > 0; i++) {
+            let idx;
+            if (map) {
+                idx = map[i];
+            }
+            else {
+                idx = Math.floor(Math.random() * 50) % tweens.length;
+            }
+            this.posTweensMap.push(idx)
+        }
+        // debugger
+    }
+
+
+    setQuaternionTweens(tweens) {
+        this.qTweens = tweens;
+        this.qTweensMap = [];
+        for (let i = 0; i < this.amount && tweens.length > 0; i++) {
+            const idx = Math.floor(Math.random() * 50) % tweens.length;
+            this.qTweensMap.push(idx)
+        }
+    } 
+
+    update() {
+        if (this.initialArrangement.length != this.amount) {
+            throw new Error("Instances number mismatch")
+        }
+        for (var i = 0; i < this.meshes.length; i++) {
+            for (var j = 0; j < this.amount; j++) {
+                // position
+                this.dummy.matrix.set(this.meshes[i].getMatrixAt(j));
+                this.dummy.updateMatrix();
+            
+                if (this.posTweensMap.length > 0) {
+                    const c = this.initialArrangement[j].clone().add(this.posTweens[this.posTweensMap[j]].target);
+                    this.dummy.position.set(c.x, c.y, c.z);
+                }
+
+                // rotations
+                if (this.qTweensMap.length > 0) {
+                    const targetRot = this.qTweens[this.qTweensMap[j]].target
+
+                    this.dummy.rotation.x = targetRot.x;
+                    this.dummy.rotation.y = targetRot.y;
+                    this.dummy.rotation.z = targetRot.z;
+                }
+                this.dummy.updateMatrix();
+                this.meshes[i].setMatrixAt(j, this.dummy.matrix)
+            }
+            this.meshes[i].instanceMatrix.needsUpdate = true
+        }
+    }
+}
+
+export default TweenableMesh;
