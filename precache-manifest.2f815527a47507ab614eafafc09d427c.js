5aa4c178b9fac17c40a4fd1ed8ff25788bba85e2
unknown <krazybubbler@gmail.com>
2019-10-15 16:16:45 +0200

    Updates

--- precache-manifest.2f815527a47507ab614eafafc09d427c.js
+++ precache-manifest.2f815527a47507ab614eafafc09d427c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "448348d99bf847ae7329f9fe86c9a939",
+    "revision": "e2553779c46054544a517a27352ee1d2",
     "url": "/three-nuts/index.html"
   },
   {
-    "revision": "6d310d993d0d19fbbd96",
+    "revision": "86ae483f6ca41aaef1d0",
     "url": "/three-nuts/static/css/main.1779617f.chunk.css"
   },
   {
-    "revision": "c1ed4676dd1789618211",
-    "url": "/three-nuts/static/js/2.ebcd9d1b.chunk.js"
+    "revision": "7a617d50a6f781a731c3",
+    "url": "/three-nuts/static/js/2.96e5ec63.chunk.js"
   },
   {
-    "revision": "6d310d993d0d19fbbd96",
-    "url": "/three-nuts/static/js/main.f3d609a0.chunk.js"
+    "revision": "86ae483f6ca41aaef1d0",
+    "url": "/three-nuts/static/js/main.b464df01.chunk.js"
   },
   {
     "revision": "8497d7fea3d5aa460471",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/three-nuts/precache-manifest.35f25f07211fdb938fd36ed867e54e7c.js"
+  "/three-nuts/precache-manifest.2f815527a47507ab614eafafc09d427c.js"
 );
 
 self.addEventListener('message', (event) => {
